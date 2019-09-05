2ed6d300ecb083b34e8325c864a81a9833616150
ferbin17 <ferbin17@gmail.com>
2019-09-05 16:17:09 +0530

    Error handling fixes

--- lib/fizzbuzz_mf.rb
+++ lib/fizzbuzz_mf.rb
@@ -1,19 +1,26 @@
 class FizzBuzz
-  def self.fizzbuzz(lower, upper)
-    if error_handler(lower, upper)
-      lower, upper = upper, lower if lower > upper
-      (lower..upper).each do |num|
+  def self.fizzbuzz(*args)
+    if error_handler(args)
+      (args[0]..args[1]).each do |num|
         fizzbuzz = ""
         fizzbuzz += "Fizz" if num % 3 == 0
         fizzbuzz += "Buzz" if num % 5 == 0
         puts (fizzbuzz == "" ? num : fizzbuzz)
       end
     else
-      puts "Error ! Change INput Values"
+      puts "Error ! Change Input Values"
     end
   end
 
-  def self.error_handler(lower, upper)
-    return (lower.is_a?(Integer) && upper.is_a?(Integer) && lower > 0 && upper > 0)
+  def self.error_handler(args)
+    begin
+      raise "Invalid Number of Arguments" unless args.length == 2
+      lower = args[0]
+      upper = args[1]
+      raise "Input is not an integer" unless lower.is_a?(Integer) && upper.is_a?(Integer)
+      raise "Input is a negative value" unless lower > -1 && upper > -1
+      raise "lower limit is higher than upper limit" unless lower < upper
+    end
+    return true
   end
 end
